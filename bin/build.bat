f22787f7e6527639b0001407c70abedec46e2786
Chig00 <chiago321@gmail.com>
2023-12-11 19:11:02 +0000

    Pivot to MiniDNN to replace opennn

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,6 @@
 call bin\clean
 mkdir build
-g++ src/main/cpp/main.cpp -o build/main -std=c++23 || goto :error
+g++ src/main/cpp/main.cpp -o build/main -std=c++23 -Ilib/opennn/eigen -Ilib/MiniDNN/include -Wall -Werror || goto :error
 goto :success
 
 :error
--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-
-#undef NOMINMAX
-#pragma GCC diagnostic ignored "-Wdeprecated"
-#include "../../../lib/opennn/opennn/opennn.h"
-#pragma GCC diagnostic pop
+#include <MiniDNN.h>
 
 int main() {
-	// opennn::Index inputs = 2;
-	// opennn::Index hidden = 4;
-	// opennn::Index outputs = 2;
-	// opennn::Tensor<Index, 1> architecture(3);
-	// architecture(0) = inputs;
-	// architecture(1) = first_hidden;
-	// architecture(2) = outputs;
-	// opennn::NeuralNetwork network(opennn::NeuralNetwork::ProjectType::Approximation, architecture);
-	// std::cout << network.get_layers_neurons_numbers();
-	opennn::NeuralNetwork network;
+	MiniDNN::Network network;
+	std::cout << network.num_layers() << std::endl;
 	return 0;
 }
