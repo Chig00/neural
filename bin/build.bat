09cb894ccc752d0e46545c3ac5d3c41ac24e035d
Chig00 <chiago321@gmail.com>
2023-12-30 20:31:03 +0000

    Update code (dimensions still need adjustment)

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,6 @@
 call bin\clean
 mkdir build
-g++ src/main/cpp/main.cpp -o build/main -std=c++23 -Ilib/eigen -Ilib/MiniDNN/include -Wall -Werror || goto :error
+g++ src/main/cpp/main.cpp -o build/main -std=c++23 -Ilib/eigen-3.4.0 -Ilib/MiniDNN/include -Wall -Werror || goto :error
 goto :success
 
 :error
--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -3,6 +3,7 @@
 #include "util.hpp"
 
 int main() {
+	std::cout << "\nGenerating data...\n";
 	RNG rng;
 	Eigen::MatrixXd train_data(100, 2);
 	Eigen::MatrixXd train_labels(100, 1);
@@ -20,6 +21,7 @@ int main() {
 		test_labels(i, 0) = test_data(i, 0) > 0.5 && test_data(i, 1) > 0.5 ? 1 : -1;
 	}
 	
+	std::cout << "\nConstructing architecture...\n";
 	MiniDNN::Network network;
 	network.add_layer(new MiniDNN::FullyConnected<MiniDNN::ReLU>(2, 4));
 	network.add_layer(new MiniDNN::FullyConnected<MiniDNN::Identity>(4, 2));
@@ -27,8 +29,12 @@ int main() {
 	MiniDNN::VerboseCallback callback;
 	network.set_callback(callback);
 	network.init();
-	MiniDNN::Adagrad optimiser;
+	
+	std::cout << "\nTraining...\n";
+	MiniDNN::AdaGrad optimiser;
 	network.fit(optimiser, train_data, train_labels, 100, 10);
+	
+	std::cout << "\nTesting...\n";
 	Eigen::MatrixXd prediction(network.predict(test_data));
 	return 0;
 }
